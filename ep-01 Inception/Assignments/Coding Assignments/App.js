23a35403ad4f4d5eeb516925a159594a96b1043f
Aditi Deokar <aditideokar123@gmail.com>
2024-11-12 16:00:36 +0530

    Coding Assignment Task-4 : Create Nested Elements in React

--- ep-01 Inception/Assignments/Coding Assignments/App.js
+++ ep-01 Inception/Assignments/Coding Assignments/App.js
@@ -1,12 +1,23 @@
-//Creating React Element
-const heading = React.createElement(
-    "h1",
-    {id:"heading"},
-    "Hello World from React!"
-);
+//Creating Nested Element
+/*
+<div id="parent">
+    <div id="child">
+    <h1>I am Aditi Deokar</h1>
+    <h2>I am ReactJS Developer </h2>
+    </div>
+</div>
+*/
 
-
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        [React.createElement("h1",{},"I am Aditi Deokar"),React.createElement("h2",{},"I am ReactJS Developer")]
+    )
+)
 
 //Creating root and rendering Elements
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
