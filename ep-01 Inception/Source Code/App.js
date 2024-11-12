3e79faa7b2903e17f7a36dcb4ecdb4fb92cd257a
Aditi Deokar <aditideokar123@gmail.com>
2024-11-12 12:18:57 +0530

    Create Nested Element Structure of HTML using React

--- ep-01 Inception/Source Code/App.js
+++ ep-01 Inception/Source Code/App.js
@@ -1,6 +1,21 @@
-const heading = React.createElement("h1",{
-    id:"heading",
-    xyz:"random attribute"
-},"Hello World! from React");
+/**
+ * 
+ * Creating Nested Element Structure of HTML in React
+ * <div id= "parent">
+ *      <div id="child">
+ *          <h1>I ma h1 tag</h1>
+ *      </div>
+ * </div>
+ * 
+ * 
+ */
+
+const parent = React.createElement("div",{id:"parent"},
+        React.createElement("div",{id:"child"},
+        React.createElement("h1",{},"I am H1 tag")
+    )
+)
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
