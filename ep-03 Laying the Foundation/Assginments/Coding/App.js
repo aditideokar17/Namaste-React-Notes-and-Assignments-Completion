88d5ee82ee3009dd74f91e51794da36fed354af1
Aditi Deokar <aditideokar123@gmail.com>
2024-11-19 14:50:03 +0530

    Solve Coding Assignment Task 7 Header with Logo Searchbar userIcon

--- ep-03 Laying the Foundation/Assginments/Coding/App.js
+++ ep-03 Laying the Foundation/Assginments/Coding/App.js
@@ -1,27 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import userIcon from "./assets/user.png";
 
-// {TitleComponent} vs {<TitleComponent/>} vs
-// {<TitleComponent></TitleComponent>} in JSX
-
-const titleElem = (
-    <h1>title React Element</h1>
-);
-
-const TitleComponent = () =>(
-    <h1>Title React Component</h1>
-)
+// Create a Header Component from scratch using Functional Components with
+// JSX
+// ○ Add a Logo on left
+// ○ Add a search bar in middle
+// ○ Add User icon on right
+// ○ Add CSS to make it look nice
 
 const Header = () =>{
     return(
-        <div>
-        <h1>{titleElem}</h1>
-        <h2><TitleComponent/></h2>
-        <h3><TitleComponent></TitleComponent></h3>
+        <div id="header">
+            <h1 id="logo">Namaste React</h1>
+            <div id="searchbar">
+                <input placeholder="Search.."></input>
+                <button>search</button>
+            </div>
+            <div id="user">
+            <img  src={userIcon} alt="User Icon"/>
+            </div>
+           
         </div>
     )
 }
-
 
 
 //rendering
