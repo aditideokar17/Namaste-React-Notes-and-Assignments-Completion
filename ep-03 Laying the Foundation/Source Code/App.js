54698bbcf4ad4afb44096ce436d62b1adef4e4b4
Aditi Deokar <aditideokar123@gmail.com>
2024-11-19 10:18:10 +0530

    Create heading using JSX

--- ep-03 Laying the Foundation/Source Code/App.js
+++ ep-03 Laying the Foundation/Source Code/App.js
@@ -1,3 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
+
+// React Element
+const heading = React.createElement("h1",{},"This is React Element");
+
+//JSX
+const jsxHeading = <h1>This heading from JSX</h1>
+
+
+
+
+// Rendering in react
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+);
+
+root.render(heading);
