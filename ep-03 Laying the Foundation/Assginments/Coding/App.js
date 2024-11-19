dfbb633707f911f971696c8ab91078459afdcafe
Aditi Deokar <aditideokar123@gmail.com>
2024-11-19 14:24:53 +0530

    Solve Coding Assignment Task 4

--- ep-03 Laying the Foundation/Assginments/Coding/App.js
+++ ep-03 Laying the Foundation/Assginments/Coding/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Create a functional component of the same with JSX
-
+// Pass attributes into the tag in JSX
 const Header = () =>{
     return(
-        <div>
+        <div id="title" className="div-color">
         <h1>heading1</h1>
         <h2>heading2</h2>
         <h3>heading3</h3>
--- ep-03 Laying the Foundation/Assginments/Coding/README.md
+++ ep-03 Laying the Foundation/Assginments/Coding/README.md
@@ -42,3 +42,14 @@ const Header = () =>{
 } -->
 
 # Task - 04
+<!-- 
+Pass attributes into the tag in JSX
+const Header = () =>{
+    return(
+        <div id="title" className="div-color">
+        <h1>heading1</h1>
+        <h2>heading2</h2>
+        <h3>heading3</h3>
+        </div>
+    )
+} -->
